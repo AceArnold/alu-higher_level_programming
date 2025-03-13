f82310e46cb1ef2ba9a833d9a7d422633ed72e16
AceArnold <a.mutara@gmail.com>
2025-03-13 04:27:13 -0700

    all in inheritance

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# a function that returns the list of available attributes and methods
+"""define 'lookup' function"""
+
+
+def lookup(obj):
+    """Return:list object"""
+    return dir(obj)
